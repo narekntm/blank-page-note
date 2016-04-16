edc478740aaa6252e8712e60aec2ee63b3dbbbc6
Aram Baghdasaryan <xquack@gmail.com>
2016-04-16 23:06:22 +0400

    Tab drawing and generation

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
+var db;
+
 $(function() {
 	setupEditor();
 	migrate();
 	setupEnv();
+
+	$('.new').click(function(e) {
+		e.preventDefault();
+
+		drawTab('New Tab');
+	});
 });
 
 $.fn.oneClickSelect = function() {
@@ -20,48 +28,95 @@ $.fn.oneClickSelect = function() {
 };
 
 function setupEnv() {
-	var content = localStorage.getItem('content'),
-			$editor = $('.editor'),
-			$source = $('.source'),
-			$preview = $('.preview'),
-			$switcher = $('.switcher'),
-			converter = new Showdown.converter();
+	var $editor = $('.editor'),
+		$source = $('.source'),
+		$preview = $('.preview'),
+		$switcher = $('.switcher'),
+		converter = new Showdown.converter();
 
-	if (!content || content == '') {
-		content = 'Don\'t make me think...';
-	}
+	drawTabs();
+	setTabActive();
 
-	$source.val(content);
+	//$source.keyup(function() {
+	//	var tabContentSource = $source.val(),
+	//		tabContentPreview = converter.makeHtml(tabContentSource),
+	//		activeTabIndex = $('.tabs li.active').attr('data-index');
+	//
+	//	content[activeTabIndex] = tabContentSource;
+	//
+	//	$preview.html(tabContentPreview);
+	//
+	//	localStorage.setItem('content', content);
+	//}).trigger('keyup');
+	//
+	//$switcher.on('click', function(e) {
+	//	e.preventDefault();
+	//
+	//	if ($switcher.text() == 'Enable') {
+	//		$editor.addClass('active');
+	//		$source.trigger('input');
+	//		$switcher.text('Disable');
+	//	} else {
+	//		$editor.removeClass('active');
+	//		$switcher.text('Enable');
+	//	}
+	//});
+	//
+	//// to force focus and change css style
+	//$('code').attr('tabindex', 0);
+	//
+	//// Select and copy on click
+	//$('code').oneClickSelect();
+}
 
-	$source.keyup(function() {
-		content = $source.val();
+function drawTabs() {
+	for (var localStorageItem in localStorage) {
+		if (localStorage.hasOwnProperty(localStorageItem)) {
+			if (localStorageItem == 'v') {
+				return true;
+			}
 
-		$preview.html(converter.makeHtml(content));
-		localStorage.setItem('content', content);
-	}).trigger('keyup');
-
-	$switcher.on('click', function(e) {
-		e.preventDefault();
-
-		if ($switcher.text() == 'Enable') {
-			$editor.addClass('active');
-			$source.trigger('input');
-			$switcher.text('Disable');
-		} else {
-			$editor.removeClass('active');
-			$switcher.text('Enable');
+			drawTab(localStorageItem);
 		}
-	});
+	}
+}
 
-	// to force focus and change css style
-	$('code').attr('tabindex', 0);
+function drawTab(name) {
+	var tab = $('.tab-template.hide').clone(),
+		clearName = sanitizeTabName(name);
 
-	// Select and copy on click
-	$('code').oneClickSelect();
+	tab
+		.removeClass('tab-template')
+		.removeClass('hide')
+		.addClass('tab')
+		.find('a').attr('href', '#' + clearName)
+		.text(name);
+
+	$('.tabs').append(tab);
+
+	return clearName;
+}
+
+function sanitizeTabName(name) {
+	return name.toLowerCase().replace(/[^a-z0-9]/i, '');
+}
+
+function setTabActive() {
+	$('.tab').eq(0).addClass('active');
 }
 
 function migrate() {
+	if (localStorage.getItem('v') == 1) {
+		localStorage.setItem('General', localStorage.getItem('content'));
+	}
 
+	if (localStorage.hasOwnProperty('account')) {
+		localStorage.removeItem('account');
+	}
+
+	localStorage.removeItem('content');
+
+	localStorage.setItem('v', 2);
 }
 
 function setupEditor() {
@@ -70,4 +125,12 @@ function setupEditor() {
 			height: (window.innerHeight - 53) + 'px'
 		});
 	}).trigger('resize');
+
+	if (!localStorage.hasOwnProperty('v')) {
+		localStorage.setItem('v', 1);
+	}
+
+	if (!localStorage.hasOwnProperty('content')) {
+		localStorage.setItem('content', 'Don\'t make me think...');
+	}
 }
